7e4a49dd598d6774aa45d8144268792d9466f601
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-05-12 01:46:19 +0600

    Input taking and String nullable-->?

---
+++ Assignment-3/Program.cs
@@ -0,0 +1,29 @@
+﻿class Assignment_3
+{
+    static void Main(string[] args)
+    {
+        
+        string? studentName;
+        int studentAge;
+        double studentGpa;
+        bool isRegistured;
+
+        Console.Write("Enter your name: ");
+        studentName = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        studentAge = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter your gpa: ");
+        studentGpa= Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Is student registered ? : ");
+        isRegistured= Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine("Name: " + studentName);
+        Console.WriteLine("Age: " + studentAge);
+        Console.WriteLine("Gpa: " + studentGpa);
+        Console.WriteLine("student registered: " + isRegistured);
+
+    }
+}
