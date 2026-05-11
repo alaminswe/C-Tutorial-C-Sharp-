22f768bec915fd9c7c3f6d268a7aa562caf83c1d
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-05-12 03:45:48 +0600

    Applying if else

---
+++ Assignment-7/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Assignment_7
+{
+    static void Main(string[] args)
+    {
+        int num1;
+        Console.Write("Enter a number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        if(num1%2!=0)
+        {
+            Console.WriteLine($"{num1} is odd number");
+        }
+        else
+        {
+            Console.WriteLine($"{num1} is even number");
+        }
+
+    }
+}
