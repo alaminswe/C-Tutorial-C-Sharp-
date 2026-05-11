51d421419f5d9ddfe671d5a5ecdf719b11aa2737
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-05-12 00:10:42 +0600

    first commit

---
+++ Assignment-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Assignment_1
+{
+    public static void Main(string[] args)
+    {
+        
+        string name = "Md. Al-Amin Ridoy.";
+        Console.WriteLine($"My Name is {name}");
+        Console.WriteLine("I am 25 years Old.");
+        Console.WriteLine("I am a full-stack developer.");
+    }
+}
---
+++ Assignment-2/Program.cs
@@ -0,0 +1,14 @@
+﻿class Assignment_2
+{
+    static void Main(string[] args)
+    {
+        string productName = "Apple iphone 14";
+        decimal productPrice = 320.5m;
+        string category = "smart phone";
+        bool isAvailable = true;
+        int productSold = 5;
+
+        Console.WriteLine($"{productName}\n{productPrice}\n{category}\n{isAvailable}\n{productSold}");
+
+    }
+}
