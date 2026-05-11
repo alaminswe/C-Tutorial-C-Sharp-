0deaf1aac69ac6913304c2bd35c684ce5d180de3
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-05-12 03:14:10 +0600

    Converting Tem f to c and c to f

---
+++ Assignment-6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+class Assignment_6
+{
+    static void Main(string[] args)
+    {
+        // //fahrenheit to celsius
+        // double fahrenheit, celsius;
+        // Console.Write("fahrenheit: ");
+        // fahrenheit = Convert.ToDouble(Console.ReadLine());
+        
+        // celsius = (fahrenheit-32)/1.8;
+        // Console.WriteLine($"celsius : {celsius:F2} degress");
+
+        //assignment celsius to fahrenheit
+        double fahrenheit, celsius;
+        Console.Write("celsius: ");
+        celsius = Convert.ToDouble(Console.ReadLine());
+        
+        fahrenheit = (1.8*celsius)+32;
+        Console.WriteLine($"fahrenheit : {fahrenheit:F2} degress");
+
+    }
+}
