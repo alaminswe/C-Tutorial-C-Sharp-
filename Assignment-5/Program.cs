503636ae6550e3d4ef8809b35f0afaa702c2b045
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-05-12 02:50:17 +0600

    Area Calculator: Triangle, Circle

---
+++ Assignment-5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+class Assignment_5
+{
+    static void Main(string[] args)
+    {
+        //Assignment area of circle
+        Console.Write("Enter redius: ");
+        double redius = Convert.ToDouble(Console.ReadLine());
+        const double pi = 3.1416;
+        
+        double area = pi * redius * redius;
+        Console.WriteLine($"area of circle: {area.ToString("F2")}");
+
+        // //Assignment area of triangleArea
+        // Console.Write("Enter base: ");
+        // int baseLength = Convert.ToInt32(Console.ReadLine());
+
+        // Console.Write("Enter height: ");
+        // int height = Convert.ToInt32(Console.ReadLine());
+
+        // const double con = 0.5;
+        
+        // double triangleArea = con * baseLength * height;
+        // Console.WriteLine($"area of triangleArea: {triangleArea.ToString("F2")}");
+        
+
+    }
+}
