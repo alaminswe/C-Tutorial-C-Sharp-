22dc0881acabb8746192a6befcbf1c56204297dd
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-06-18 02:31:50 +0600

    Adapter Pattern

---
+++ StructuralPatternsAdvancedOOPArchitecture/AdapterPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var laptop = new Laptop();
+        var europeanPlug = new EuropeanPlug();
+        var adapter = new SocketAdapter(europeanPlug);
+
+        laptop.Charge(adapter);
+    }
+}
+
+public class EuropeanPlug
+{
+    public string GetEuropeanSocket()
+    {
+        return "European Socket";
+    }
+}
+
+// Target interface expected by client
+public interface IUSASocket
+{
+    string GetUSASocket();
+}
+
+//Adepter Cls
+public class SocketAdapter: IUSASocket
+{
+    private readonly EuropeanPlug _europeanPlug;
+
+    public SocketAdapter(EuropeanPlug europeanPlug)
+    {
+        _europeanPlug = europeanPlug;
+    }
+    public string GetUSASocket()
+    {
+        return  $"Adapter converting: {_europeanPlug.GetEuropeanSocket()}";
+    }
+}
+
+public class Laptop
+{
+    public void Charge(IUSASocket socket)
+    {
+        Console.WriteLine($"Cherger Pluged in : {socket.GetUSASocket()}");
+    }
+}
