1bf226ea5681c3f621fa26ca2c3483d7854fbe54
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-06-17 21:16:52 +0600

    Creational Pattern

--- Assignment-1/obj/Debug/net10.0/Assignment-1.AssemblyInfo.cs
+++ Assignment-1/obj/Debug/net10.0/Assignment-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3fef3a90eeb517c03f9e1f420d4a9f364c4847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bf0c39c6a905b7ef117f98653c06d6545a34c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CreationalPatterns/Observer/Program.cs
+++ CreationalPatterns/Observer/Program.cs
@@ -1,1 +1,89 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var channel1 = new NewsChannel("NTV");
+        var channel2 = new NewsChannel("Somoy TV");
+        var mobileApp = new MobileApp();
+
+        var agency = new NewsAgency();
+
+        agency.Subscribe(channel1);
+        agency.Subscribe(channel2);
+        agency.Subscribe(mobileApp);
+
+
+        agency.SetNews("The Observer Pattern works!");
+
+        Console.WriteLine();
+        agency.Unsubscribe(channel2);
+
+        agency.SetNews("Breaking: Bangladesh wins!");
+    }
+}
+
+public interface IObserver
+{
+    void Update(string message);
+}
+
+public interface ISubject
+{
+    void Subscribe(IObserver observer);
+    void Unsubscribe(IObserver observer);
+    void NotifyAll(string message);
+}
+
+public class NewsChannel : IObserver
+{
+    private string _channelName;
+
+    public NewsChannel(string name)
+    {
+        _channelName = name;
+    }
+
+    public void Update(string message)
+    {
+        Console.WriteLine($"📺 [{_channelName}] Breaking: {message}");
+    }
+}
+
+public class MobileApp : IObserver
+{
+    public void Update(string message)
+    {
+        Console.WriteLine($"📱 [Mobile App] Push Alert: {message}");
+    }
+}
+
+public class NewsAgency : ISubject
+{
+    private readonly List<IObserver> _observers = new();
+    private string _latestNews;
+
+    public void Subscribe(IObserver observer)
+    {
+        _observers.Add(observer);
+        Console.WriteLine($"✅ New subscriber ! Total: {_observers.Count}");
+    }
+    public void Unsubscribe(IObserver observer)
+    {
+        _observers.Remove(observer);
+        Console.WriteLine($"❌ Unsubscribed! Remaining: {_observers.Count}");
+    }
+    public void NotifyAll(string message)
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Update(message);
+        }
+    }
+
+    public void SetNews(string news)
+    {
+        _latestNews = news;
+        Console.WriteLine($"\n🗞️ NewsAgency published: '{news}'");
+        NotifyAll(news);
+    }
+}
